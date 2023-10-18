bc02df855331d90fd9553f042b6fdd7cabd95c58
Kgomotso222 <138695830+Kgomotso222@users.noreply.github.com>
2023-10-19 01:51:17 +0200

    Update filer.c

--- filer.c
+++ filer.c
@@ -1,4 +1,9 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
  * main - entry point
@@ -11,20 +16,14 @@ int main(int ac, char **av)
 {
     custom_info_t info;
     custom_info_init(&info);
-    int fd = 2;
-
-    asm("mov %1, %0\n\t"
-        "add $3, %0"
-        : "=r"(fd)
-        : "r"(fd));
 
     if (ac == 2)
     {
-        fd = open(av[1], O_RDONLY);
+        int fd = open(av[1], O_RDONLY);
         if (fd == -1)
         {
             if (errno == EACCES)
-                return (126);
+                return 126;
             if (errno == ENOENT)
             {
                 custom_eputs(av[0]);
@@ -32,12 +31,13 @@ int main(int ac, char **av)
                 custom_eputs(av[1]);
                 custom_eputchar('\n');
                 custom_eputchar(BUF_FLUSH);
-                return (127);
+                return 127;
             }
             return EXIT_FAILURE;
         }
         info.readfd = fd;
     }
+
     populate_custom_env_list(&info);
     read_history_custom(&info);
     hsh(&info, av);
